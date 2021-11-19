66d56cb129931c531bbba5ac58975094b87c5e51
gabhijeetaxioned <abhijeetg@axioned.com>
2021-11-19 11:49:26 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// const inputName = ['ID','Name','Age','Dept'];
 
 function App() {
+  
+  const [data, setdata] = useState({
+    "ID": null,
+    "fname": null,
+    "age": null,
+    "dept": null
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <div className='form-group'>
+          <label>ID : </label>
+          <input type='text' onChange={e => setdata({
+            "ID": e.target.value
+          })}></input>
+        </div>
+        <div className='form-group'>
+          <label>Name : </label>
+          <input type='text' onChange={e => setdata({
+            "fname": e.target.value
+          })}></input>
+        </div>
+        <div className='form-group'>
+          <label>Age : </label>
+          <input type='text' onChange={e => setdata({
+            "age": e.target.value
+          })}></input>
+        </div>
+        <div className='form-group'>
+          <label>Dept : </label>
+          <input type='text' onChange={e => setdata({
+            "dept": e.target.value
+          })}></input>
+        </div>
+        <div className='form-control'>
+          <button onClick={() => display(data)}>Submit</button>
+        </div>
+      </form>
+
+      {
+        display(data)
+      }
+    </>
   );
+}
+
+
+const display = (props) => {
+ 
+  const getData = {
+    "ID": props.ID,
+    "fname": props.fname
+  }
+
+  console.log(getData);
+  // return (
+  //   <div>
+  //     <h2>Emp. ID : {data.ID}</h2>
+  //     <h2>Name : {data.fname}</h2>
+  //     <h3>Age : {data.age}</h3>
+  //     <h3>Dept : {data.dept}</h3>
+  //   </div>
+  // )
 }
 
 export default App;
